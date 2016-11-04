d667efa942013ca2e43d437b78646f35e942b1a9
Jeffrey D Huskey <jdhuskey@mac.com>
2016-11-04 17:33:08 -0400

    Completed Firebase email authentication.

--- Share/SignInVC.swift
+++ Share/SignInVC.swift
@@ -13,6 +13,10 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: CustomTextField!
+    @IBOutlet weak var passwordField: CustomTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -41,9 +45,7 @@ class SignInVC: UIViewController {
                 self.firebaseAuth(credential)
                 
             }
-            
         }
-        
     }
     
     func firebaseAuth(_ credential: FIRAuthCredential) {
@@ -60,5 +62,33 @@ class SignInVC: UIViewController {
         })
     }
 
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let password = passwordField.text {
+            
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                
+                if error == nil {
+                    
+                    print("JEFF: Email user successfully authenticated with Firebase")
+                    
+                } else {
+                    
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        
+                        if error != nil {
+                            
+                            print("JEFF: Email user unable to authenticate with Firebase - \(error)")
+                            
+                        } else {
+                            
+                            print("JEFF: Email user successfully created and authenticated with Firebase")
+                            
+                        }
+                    })
+                }
+            })
+        }
+    }
 }
 
