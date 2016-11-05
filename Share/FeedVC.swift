7997cc1045a4636dc13bd9f8d545276a9ae8c5d3
Jeffrey D Huskey <jdhuskey@mac.com>
2016-11-05 18:14:23 -0400

    Completed display of text values and like count.

--- Share/FeedVC.swift
+++ Share/FeedVC.swift
@@ -63,11 +63,21 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print("JEFF: \(post.caption)")
         
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            
+            cell.configureCell(post: post)
+            return cell
+            
+        } else {
         
+            return PostCell()
+            
+        }
+        
+        //return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         //return UITableViewCell()
+        
     }
     
     @IBAction func signOutTapped(_ sender: Any) {
--- Share/PostCell.swift
+++ Share/PostCell.swift
@@ -16,10 +16,20 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLabel: UILabel!
     @IBOutlet weak var likedImage: UIImageView!
+    
+    var post: Post!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func configureCell(post: Post) {
+        
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLabel.text = "\(post.likes)"
+        
+    }
+    
 }
