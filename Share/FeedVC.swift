1fae22e974b4a3f0999ad861d27ade6fd1a589f8
Jeffrey D Huskey <jdhuskey@mac.com>
2016-11-05 17:58:06 -0400

    Now Firebase get is truly complete, though we still have to load the data into the table cell.

--- Share/FeedVC.swift
+++ Share/FeedVC.swift
@@ -15,6 +15,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +26,29 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
         
-            print(snapshot.value as Any)
-        
-        })
+            //print(snapshot.value as Any)
             
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                
+                for snap in snapshot {
+                    
+                    print("SNAP: \(snap)")
+                    
+                    if let postDictionary = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDictionary)
+                        
+                        self.posts.append(post)
+                        
+                    }
+                    
+                }
+                
+            }
         
+            self.tableView.reloadData()
+            
+        })
     
     }
     
@@ -37,10 +57,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("JEFF: \(post.caption)")
         
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         
